9083b6ce52fcd58970087697952c0e96e84f391b
fernandoprestes <fernandoprestesc@gmail.com>
2022-05-28 12:41:51 -0300

    feat: add buscar por nome

--- src/pages/Store/index.vue
+++ src/pages/Store/index.vue
@@ -1,13 +1,14 @@
 <script setup>
   import { onMounted, ref, watch } from 'vue';
-  import useFetchCakes from './../../composables/useFetchCakes.js';
+  import useFetchCakes from '@/composables/useFetchCakes.js';
+  import Icon from '@/components/Icon/Icon.vue';
 
   const { fetchCakes, fetchCakesQuery } = useFetchCakes();
 
   const cakes = ref(null);
-
   const selectedOption = ref('todos');
   const filterOptions = ['todos', 'chocolate', 'especial', 'simples'];
+  const search = ref(null);
 
   async function initFetch() {
     const { data } = await fetchCakes();
@@ -15,6 +16,7 @@
   }
 
   watch(selectedOption, async () => {
+    if (!selectedOption.value) return;
     if (selectedOption.value === 'todos') {
       initFetch();
       return;
@@ -23,19 +25,36 @@
     cakes.value = data;
   });
 
+  const handleSearchClick = async () => {
+    if (!search.value) return;
+    selectedOption.value = null;
+    const { data } = await fetchCakesQuery(`name=${search.value}`);
+    cakes.value = data;
+  };
+
   onMounted(async () => {
     await initFetch();
   });
 </script>
 <template>
-  <header class="h-20"></header>
-  <main class="container flex h-[calc(100vh-80px)] w-full gap-10 bg-slate-400 py-10 px-4">
-    <aside class="h-full w-80 rounded-lg bg-slate-200">
-      <h2 class="py-4 text-center">Filtrar por {{ selectedOption }}</h2>
-      <div class="px-4">
-        <div
+  <header class="h-20">
+    <div class="container flex h-full items-center justify-between px-2">
+      <h2>Logos</h2>
+      <div>
+        <Icon name="ShoppingCart" />
+      </div>
+    </div>
+  </header>
+  <main class="container flex w-full flex-col gap-4 py-4 px-4 md:flex-row md:py-10">
+    <aside class="w-full rounded-lg bg-slate-200 py-4 md:h-full md:w-80">
+      <h2 class="pb-4 text-center">Filtrar por:</h2>
+      <div class="flex gap-2 px-4 md:flex-col">
+        <label
           v-for="(item, index) in filterOptions"
           :key="index"
+          :for="item"
+          class="flex h-8 cursor-pointer items-center justify-center rounded-lg border bg-slate-300 px-1 text-sm capitalize leading-6 hover:font-bold"
+          :class="{ ' border-slate-800 font-bold': selectedOption === item }"
         >
           <input
             :id="item"
@@ -45,37 +64,44 @@
             :value="item"
             class="sr-only"
           />
-          <label
-            :for="item"
-            class="flex h-8 cursor-pointer items-center justify-center rounded-lg px-1 text-sm leading-6 shadow-2xl"
-            :class="{ 'border-2 border-slate-800': selectedOption === item }"
-          >
-            {{ item }}
-          </label>
-        </div>
+          {{ item }}
+        </label>
       </div>
     </aside>
-    <div class="flex h-full w-full flex-col gap-2 rounded-lg bg-slate-200 p-2">
-      <div class="py-2">
-        <input type="text" />
-        <button>Buscar</button>
+    <div class="flex w-full flex-col gap-2 rounded-lg">
+      <div class="space-x-4 rounded-lg bg-slate-200 py-2 px-2">
+        <input
+          v-model="search"
+          type="text"
+          class="rounded-lg px-2 py-1"
+          placeholder="Digite o nome de um bolo..."
+        />
+        <button
+          class="rounded-lg bg-slate-500 px-4 py-1"
+          @click="handleSearchClick"
+        >
+          Buscar
+        </button>
       </div>
-      <div class="flex flex-wrap gap-10">
+
+      <div class="flex flex-wrap justify-center gap-4 md:justify-between lg:justify-start">
         <div
           v-for="(item, index) in cakes"
           :key="index"
-          class="h-60 w-60 rounded-lg"
+          class="w-[194px] rounded-lg border border-slate-200 sm:w-[186px] md:w-52"
         >
-          <div class="relative">
+          <div class="h-28">
             <img
-              class="h-40 w-full rounded-tr-lg rounded-tl-lg"
+              class="h-full w-full rounded-tr-lg rounded-tl-lg"
               src="https://img.freepik.com/fotos-gratis/bolo-de-chocolate-com-granulado-de-chocolate_144627-8998.jpg?w=2000"
               alt=""
             />
-            <div
-              class="absolute right-2 bottom-1 rounded-lg border border-white bg-slate-600 px-2 text-sm text-white ring-2 ring-slate-600"
-            >
-              {{ item.category }}
+            <div class="relative">
+              <div
+                class="absolute right-2 bottom-1 rounded-lg border border-white bg-slate-600 px-2 text-sm text-white ring-2 ring-slate-600"
+              >
+                {{ item.category }}
+              </div>
             </div>
           </div>
 
