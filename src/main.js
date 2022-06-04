8a0bf57eaa0eb4af2f90bd69d77aaa1099b6f009
fernandoprestes <fernandoprestesc@gmail.com>
2022-06-04 16:36:00 -0300

    project(package): add @vueuse/head

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { createHead } from '@vueuse/head';
 import App from './App.vue';
 import './main.css';
 import router from './router';
 import 'animate.css';
 
 const pinia = createPinia();
+const head = createHead();
 
-createApp(App).use(router).use(pinia).mount('#app');
+createApp(App).use(router).use(pinia).use(head).mount('#app');
