c6b9e118bc1993372765b6c74c4e363ae2411b4d
fernandoprestes <fernandoprestesc@gmail.com>
2022-06-04 13:22:16 -0300

    feat: adicionado tailwind colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     extend: {
       colors: {
         overlay: 'rgba(0, 0, 0, 0.5)',
+        primary: '#9387AB',
+        secondary: '#F9F1F6',
+        accent: '#E7C5DA',
       },
     },
   },
